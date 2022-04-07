85c31ac0b16e6ff256facfb9266800af67fb0c7b
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-07 17:01:54 -0500

    copy exercises into js file and complete exercises

--- random-exercises.js
+++ random-exercises.js
@@ -1,3 +1,4 @@
+'use strict'
 // var a = 1
 // var b = a++
 // var c = ++a
@@ -143,3 +144,34 @@
 // // redo code to function look at justin's code
 
 
+// ================ !! Mini-exercises !!
+
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+ function returnFive() {
+     return 5;
+ }
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+function isFive(input){
+    return input == 5;
+}
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+function isShortWord(string){
+    return string.length < 5;
+}
+console.log(isShortWord('password'), false)
+console.log(isShortWord('heff'), true)
+console.log(isShortWord(''), true)
+console.log(isShortWord('fives'), false)
+// format to test work
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+function isSameLength(inOne, inTwo){
+    var wordsEqual = inOne.length == inTwo.length
+    return wordsEqual
+}
+// Write a function, getSmallerSegment, that takes in a string and a number input. The function should return a substring of the first argument that is as many characters long as the second argument in lowercase.
+function getSmallerSegment(inputString, inputnumber){
+    var cutWord = inputnumber == inputString
+    return cutWord
+}
+// example input: getSmallerSegment("Codeup", 3)
+// example output: "cod"
