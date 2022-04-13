6f4d75c6ea763a7a98cd35dcd97c5e06800ae0eb
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-12 22:07:19 -0500

    create files for loop exercises and complete questions 1-5

---
+++ js/for_loops.js
@@ -0,0 +1,31 @@
+(function(){
+    'use strict'
+    //question 2
+    // function showMultiplicationTable(num){
+    //     var total = num * 7
+    //     return console.log('7 * ' + num + ' = ' + total)
+    // }
+    // showMultiplicationTable(1)
+    // showMultiplicationTable(5)
+    // showMultiplicationTable(10)
+
+// question 3
+for(var i = 1;i <= 10; i++){
+    var random = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+    if(random % 2 === 0){
+        console.log(random + ' is even')}
+    if(random % 2 !== 0){
+        console.log(random + ' is odd')
+    }
+}
+// question 4
+    for(var i = 1;i < 10; i++){
+        console.log(i)
+    }
+// question 5
+    for(var x = 100; x >=1; x -= 5){
+        console.log(x)
+    }
+
+
+})();
