21772dab95246512a495df9803eb784c242b3b05
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-13 10:01:26 -0500

    complete warm ups 13 apr

--- random-exercises.js
+++ random-exercises.js
@@ -1,4 +1,4 @@
-'use strict'
+
 // var a = 1
 // var b = a++
 // var c = ++a
@@ -147,34 +147,65 @@
 // ================ !! Mini-exercises !!
 
 // Write a function, returnFive, that returns the number five. No inputs should be defined.
- function returnFive() {
-     return 5;
- }
+//  function returnFive() {
+//      return 5;
+//  }
 // Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
-function isFive(input){
-    return input == 5;
-}
+// function isFive(input){
+//     return input == 5;
+// }
 // Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
-function isShortWord(string){
-    return string.length < 5;
-}
-console.log(isShortWord('password'), false)
-console.log(isShortWord('heff'), true)
-console.log(isShortWord(''), true)
-console.log(isShortWord('fives'), false)
+// function isShortWord(string){
+//     return string.length < 5;
+// }
+// console.log(isShortWord('password'), false)
+// console.log(isShortWord('heff'), true)
+// console.log(isShortWord(''), true)
+// console.log(isShortWord('fives'), false)
 // format to test work
 // Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
-function isSameLength(inOne, inTwo){
-    var wordsEqual = inOne.length == inTwo.length
-    return wordsEqual
-}
+// function isSameLength(inOne, inTwo){
+//     var wordsEqual = inOne.length == inTwo.length
+//     return wordsEqual
+// }
 // Write a function, getSmallerSegment, that takes in a string and a number input. The function should return a substring of the first argument that is as many characters long as the second argument in lowercase.
-function getSmallerSegment(str, segmentLength){
-    return str.substring(0, segmentLength).toLowerCase()
+// function getSmallerSegment(str, segmentLength){
+//     return str.substring(0, segmentLength).toLowerCase()
+// }
+//
+// getSmallerSegment("Codeup", 3)
+
+// Define a function named `isAString` that takes in a value and returns true if the input is a string, or false if the input is not a string.
+function isAString( input){
+ return typeof input === 'string';
 }
 
-getSmallerSegment("Codeup", 3)
+    // Define a function named `isNotANumber` that takes in a value and returns true if the input is not a number, or false if the input is a number. This function should consider that numeric strings are not numbers.
+function isNotANumber(input){
+ return typeof input !== 'number'
+}
+    // Define a function named `canBeANumber` that takes in a value and returns true if the input is a number or a numeric string, or false if the input is not a string or if it cannot be parsed to a number.
+function canBeANumber(input) {
+ return !isNaN(parseFloat(input))
+}
+    // Define a function named `multiplyByTen` that takes in a value, multiplies it by ten, and returns the result. If the input is not a number and is not a numeric string, it returns false.
+function multiplyByTen(input){
+ return typeof (input * 10) === 'number';
+}
+    // Write a function named `randomNumber` that takes in an input and returns a random number between zero and the value of the input.
+function randomNumber(input){
+ return (Math.random() + input)
+}
+    // Write a function that takes in a string and returns the length of the string. If the input is not a string, it should be converted to a string and still return the length of the string.
+function stringLength(input){
+ return input.toString().length;
+}
 
-// (function (){
-//     IIFE syntax
-// })();
+    // Write a function that takes in two strings. The second string should be a single character. The function should return the index of the second input in the first input. If the second input is more than one character or is not in the first input at all, it should return false.
+function indexOfCharacter(in1,in2){
+ if(in2.length > 1 || in1.indexOf(in2) === -1){
+  return false
+ }
+ return in1.indexOf(in2);
+}
+
