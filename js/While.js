7b79fdd8ca5d194fe776ad7ff39d984c5db16fec
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-13 21:32:29 -0500

    add changes to final while loop

--- js/While.js
+++ js/While.js
@@ -8,7 +8,7 @@ var i = 2;
 
 
 //     // This is how you get a random number between 50 and 100
-    var totalCones = Math.floor(Math.random() * 50) + 50;
+    var allCones = Math.floor(Math.random() * 50) + 50;
 //     console.log(i)
 //     do{
 // // // This expression will generate a random number between 1 and 5
@@ -18,13 +18,13 @@ var i = 2;
 //     }
 //     while(i >= 0)
     function randomSales(){
-        var conesBought = Math.floor(Math.random() * 5) + 1;
+        return  Math.floor(Math.random() * 5) + 1;
     }
     function calculateCones(totalCones) {
         do {
-            var totalToSell = randomSales();
+            var totalToSell = Math.floor(Math.random() * 50) + 50;
 
-            var actualSoldAmount = (totalCones >= totalToSell) ? totalToSell : totalCones
+            var actualSoldAmount = (totalCones >= totalToSell) ? totalToSell : totalCones;
 
             if (totalToSell === actualSoldAmount) {
                 console.log(actualSoldAmount + ' cones sold...')
@@ -33,7 +33,8 @@ var i = 2;
             }
             totalCones -= actualSoldAmount
 
-        } while (totalCones <= 0)
+        } while (totalCones > 0)
         console.log('yay i sold them all!')
     }
+    calculateCones(allCones)
 })();
