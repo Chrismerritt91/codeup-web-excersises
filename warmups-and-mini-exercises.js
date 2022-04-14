ca2c8892a189db5da160784dd90b06466ce4247f
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-14 11:54:08 -0500

    add problems to warmups

--- warmups-and-mini-exercises.js
+++ warmups-and-mini-exercises.js
@@ -175,37 +175,59 @@
 //
 // getSmallerSegment("Codeup", 3)
 
-// Define a function named `isAString` that takes in a value and returns true if the input is a string, or false if the input is not a string.
-function isAString( input){
- return typeof input === 'string';
-}
+// // Define a function named `isAString` that takes in a value and returns true if the input is a string, or false if the input is not a string.
+// function isAString( input){
+//  return typeof input === 'string';
+// }
+//
+//     // Define a function named `isNotANumber` that takes in a value and returns true if the input is not a number, or false if the input is a number. This function should consider that numeric strings are not numbers.
+// function isNotANumber(input){
+//  return typeof input !== 'number'
+// }
+//     // Define a function named `canBeANumber` that takes in a value and returns true if the input is a number or a numeric string, or false if the input is not a string or if it cannot be parsed to a number.
+// function canBeANumber(input) {
+//  return !isNaN(parseFloat(input))
+// }
+//     // Define a function named `multiplyByTen` that takes in a value, multiplies it by ten, and returns the result. If the input is not a number and is not a numeric string, it returns false.
+// function multiplyByTen(input){
+//  return typeof (input * 10) === 'number';
+// }
+//     // Write a function named `randomNumber` that takes in an input and returns a random number between zero and the value of the input.
+// function randomNumber(input){
+//  return (Math.random() + input)
+// }
+//     // Write a function that takes in a string and returns the length of the string. If the input is not a string, it should be converted to a string and still return the length of the string.
+// function stringLength(input){
+//  return input.toString().length;
+// }
+//
+//     // Write a function that takes in two strings. The second string should be a single character. The function should return the index of the second input in the first input. If the second input is more than one character or is not in the first input at all, it should return false.
+// function indexOfCharacter(in1,in2){
+//  if(in2.length > 1 || in1.indexOf(in2) === -1){
+//   return false
+//  }
+//  return in1.indexOf(in2);
+// }
 
-    // Define a function named `isNotANumber` that takes in a value and returns true if the input is not a number, or false if the input is a number. This function should consider that numeric strings are not numbers.
-function isNotANumber(input){
- return typeof input !== 'number'
-}
-    // Define a function named `canBeANumber` that takes in a value and returns true if the input is a number or a numeric string, or false if the input is not a string or if it cannot be parsed to a number.
-function canBeANumber(input) {
- return !isNaN(parseFloat(input))
-}
-    // Define a function named `multiplyByTen` that takes in a value, multiplies it by ten, and returns the result. If the input is not a number and is not a numeric string, it returns false.
-function multiplyByTen(input){
- return typeof (input * 10) === 'number';
-}
-    // Write a function named `randomNumber` that takes in an input and returns a random number between zero and the value of the input.
-function randomNumber(input){
- return (Math.random() + input)
-}
-    // Write a function that takes in a string and returns the length of the string. If the input is not a string, it should be converted to a string and still return the length of the string.
-function stringLength(input){
- return input.toString().length;
-}
+// ============================= ARRAYS AND FUNCTIONS
 
-    // Write a function that takes in two strings. The second string should be a single character. The function should return the index of the second input in the first input. If the second input is more than one character or is not in the first input at all, it should return false.
-function indexOfCharacter(in1,in2){
- if(in2.length > 1 || in1.indexOf(in2) === -1){
-  return false
- }
- return in1.indexOf(in2);
-}
+// !! Write a function, logNums, that takes in a array and logs each number in the array !!
 
+// !! Write a function, returnLongString, that takes in an array of strings and returns all strings concatenated together !!
+
+// !! Write a function, returnArrSum, that takes in an array of values and returns the sum of all number elements !!
+
+/* !! Write a function, checkIfNumInArray, that takes in an array of numbers and a number and returns the string
+        'input num in input array' if the second argument is an element in the first argument array
+        otherwise, the function should return the string 'input num NOT in input array'
+   !!
+
+   // !! Write a function, returnLongString, that takes in an array of strings and returns all strings concatenated together !!
+
+// !! Write a function, returnArrSum, that takes in an array of values and returns the sum of all number elements !!
+
+/* !! Write a function, checkIfNumInArray, that takes in an array of numbers and a number and returns the string
+        'input num in input array' if the second argument is an element in the first argument array
+        otherwise, the function should return the string 'input num NOT in input array'
+   !!
+ */
