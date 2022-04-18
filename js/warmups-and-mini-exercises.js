f1b5c9771135316098361a1aa560e934ebbdef98
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-04-17 20:32:21 -0500

    refactor name of exersize file and create lesson files for manipulating arrays and complete exercises

--- js/warmups-and-mini-exercises.js
+++ js/warmups-and-mini-exercises.js
@@ -231,16 +231,88 @@
 //         otherwise, the function should return the string 'input num NOT in input array'
 //
 
-var numbers = [4,13,10,7,5]
-//solve with while loop
-function numInArrayWhile(numbers, num){
-    var flag = false;
-    var i = numbers.length - 1;
-    while(i >=0){
-        if(numbers[i] === num) {
-            flag = true;
+// var numbers = [4,13,10,7,5]
+// //solve with while loop
+// function numInArrayWhile(numbers, num){
+//     var flag = false;
+//     var i = numbers.length - 1;
+//     while(i >=0){
+//         if(numbers[i] === num) {
+//             flag = true;
+//         }
+//         i--
+//     }
+//     return flag === true ? 'input num in input array' : 'input num not in input array';
+// }
+
+
+// // in class example for manipulating arrays
+// var pies = [
+//     "apple",
+//     "cherry",
+//     "key lime",
+//     "huckleberry",
+//     "rhubarb"
+// ];
+// console.log(pies)
+// // not flexible
+// // function bakedPies(pies){
+// //     return pies.slice(2)
+// // }
+// // flexible
+// function bakedPies(pies) {
+//     return pies.slice(pies.length - 3)
+// }
+// console.log(bakedPies(pies))
+
+// // ** create a function that takes in an array of pies baked and return the 3 most recently baked pies
+//
+// ============== BONUS 1
+//
+// // ** Create a function that will take in a formatted string of numbers and return an array of phone numbers without any symbols. Console.log the output of the returned array.
+//
+// /* EXAMPLE...
+//
+//     var phoneNumbers = '210-555-2020\n230-555-2020\n512-555-3030';
+//
+//     cleanPhoneNumbers(phoneNumbers);
+//
+//     the above code should output the following...
+//
+//        2105552020
+//        2305552020
+//        5125553030
+//
+//
+// */
+//
+// // var phoneNumbers = '210-555-2020\n230-555-2020\n512-555-3030';
+//
+// ============== BONUS 2
+//
+// Write a program to toggle all characters in a string: in a given string, toggle all characters to their opposite case. Special characters will not change.
+
+//     Example input: "aBc12#"
+// Example output: "AbC12#"
+//
+function swapCase(str){
+    var output = '';
+    str.split('').forEach(function(character){
+        if(character === character.toUpperCase()){
+            output += character.toLowerCase();
+        } else {
+            output += character.toUpperCase();
         }
-        i--
-    }
-    return flag === true ? 'input num in input array' : 'input num not in input array';
+    });
+    return output;
 }
+console.log(swapCase("aBc12#"))
+console.log( "AbC12#")
+// ============== BONUS 3
+//
+// Write a program that takes a given string appended with a number at the end. The program will check the length of the string to verify the string is equal to the number appended. If the number appended equals the string length, output "Yes". if the number appended does not equal the string length, output "No".
+//
+//     Example Input: "geek5"
+// Example Output: "Yes"
+//
+// Example Input: "codingchallenge25"
