ef2fa55caf2c2ac89623c4d27c4753a1fdd7f5d0
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-05-20 11:31:35 -0500

    create files for weather map project and lay first templates

--- js/weather-map.js
+++ js/weather-map.js
@@ -12,12 +12,12 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
 }).done(function(data) {
     console.log('The entire response:', data);
     console.log('Diving in - here is current information: ', data.current);
-    console.log(data.current.weather[0].description)
-    data.daily.forEach(function(dailyforecast, index){
-        if(index < 5)
-        console.log(dailyforecast.temp.max)
-        console.log(dailyforecast.temp.min)
-    })
+    // console.log(data.current.weather[0].description)
+    // data.daily.forEach(function(dailyforecast, index){
+    //     if(index < 5)
+    //     console.log(dailyforecast.temp.max)
+    //     console.log(dailyforecast.temp.min)
+    // })
 
-    // console.log('A step further - information for tomorrow: ', data.daily[1]);
+    console.log('A step further - information for tomorrow: ', data.daily[1]);
 });
