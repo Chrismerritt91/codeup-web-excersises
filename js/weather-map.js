1894ab859dd5608e384ef80df2a870cc424f9a04
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-05-20 16:22:35 -0500

    make changes to code and place lets into loop

--- js/weather-map.js
+++ js/weather-map.js
@@ -11,44 +11,39 @@ $.get("http://api.openweathermap.org/data/2.5/onecall", {
     units: "imperial"
 }).done(function(data) {
     console.log('The entire response:', data);
-    // console.log('Diving in - here is current information: ', data.current);
-    // console.log(data.current.weather[0].description)
-    // data.daily.forEach(function(dailyforecast, index){
-    //     if(index < 5)
-    //     console.log(dailyforecast.temp.max)
-    //     console.log(dailyforecast.temp.min)
-    // })
-    // console.log('A step further - information for tomorrow: ', data.daily[1]);
+    for (let i = 0; i < data.length; i++) {
+        if (i < 5)
 
-    //displays date in spot on card
-    let date = new Date(data.daily[0].dt * 1000).toDateString()
-    $(".card-date").html(date)
+            //displays date in spot on card
+            let date = new Date(data.daily[i].dt * 1000).toDateString()
+        $(".card-date").html(date)
 
-    //displays the temperature
-    let tempMin = data.daily[0].temp.min;
-    let tempMax = data.daily[0].temp.max;
-    let dailyTemp = parseInt(tempMin) + '' + '°f / ' + '' + parseInt(tempMax) + '°f';
-    $(".card-temp").html(dailyTemp);
+        //displays the temperature
+        let tempMin = data.daily[i].temp.min;
+        let tempMax = data.daily[i].temp.max;
+        let dailyTemp = parseInt(tempMin) + '' + '°f / ' + '' + parseInt(tempMax) + '°f';
+        $(".card-temp").html(dailyTemp);
 
-    //display description
-    let weatherCondition = data.daily[0].weather[0].description;
-    $(".card-descript").html("Description: " + weatherCondition);
+        //display description
+        let weatherCondition = data.daily[i].weather[0].description;
+        $(".card-descript").html("Description: " + weatherCondition);
 
-    //display humidity
-    let humidity = data.daily[0].humidity;
-    $(".card-humidity").html("Humidity: " + humidity);
+        //display humidity
+        let humidity = data.daily[i].humidity;
+        $(".card-humidity").html("Humidity: " + humidity);
 
-    //display wind speed
-    let wind = data.daily[0].wind_speed;
-    $(".card-wind").html("Wind speed: " + wind);
+        //display wind speed
+        let wind = data.daily[i].wind_speed;
+        $(".card-wind").html("Wind speed: " + wind);
 
-    //display pressure
-    let pressure = data.daily[0].pressure;
-    $(".card-pressure").html("Pressure: " + pressure);
+        //display pressure
+        let pressure = data.daily[i].pressure;
+        $(".card-pressure").html("Pressure: " + pressure);
 
 
-    let dailyIcon = data.daily[0].weather[0].icon;
-    let iconLink = "http://openweathermap.org/img.wn/" + dailyIcon + "@2x.png";
-    $(".weather-icon").src.html(iconLink)
+        let dailyIcon = data.daily[i].weather[0].icon;
+        let iconLink = "http://openweathermap.org/img/wn/" + dailyIcon + "@2x.png";
+        $(".weather-icon").attr("src", iconLink)
+    }
 
 });
